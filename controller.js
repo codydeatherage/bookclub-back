99d15ce43c2fb6af1e3b056d289e7a1c699fe25a
codydeatherage <cdeatherage14@gmail.com>
2021-08-22 23:26:54 -0700

    password authentication nearly working

--- controller.js
+++ controller.js
@@ -4,20 +4,33 @@ const Account = require('./models/account')
 const { MongoClient } = require("mongodb")
 const db = require('./db')
 const cache_ttl = 3600;
+const salt = bcrypt.genSaltSync(10);
+function compare(currPass, hashed) {
+    bcrypt.compare(currPass, hashed, (err, res)=>{
+        console.log(`comparing: ${currPass} || ${hashed}`)
+    })
+}
 
 const login = async (req, res) => {
     const body = req.body;
-    const account = await Account.findOne({username: body.username});
-    if(account){
-        const validPass = await bcrypt.compare(body.pass, user.pass);
-        if(validPass){
-             return res.status(200).json({message: "Password match"});
-        }else{
-            return res.status(400).json({error: "Password incorrect"});
+    const account = await Account.findOne({ username: body.username });
+    if (account) {
+        body.pass = bcrypt.hash(body.pass.toString(), salt, (err, res)=>{
+            console.log('hash', res);
+            hash = res;
+            compare(body.pass.toString(), account.pass);
+        })
+        const validPass = compare(body.pass, account.pass);
+        if (validPass) {
+            console.log('Account checks out')
+            return res.status(200).json({ message: "Password match" });
+        } else {
+            console.log('no account match')
+            return res.status(400).json({ error: "Password incorrect" });
         }
     }
-    else{
-        return res.status(401).json({error: "User does not exist"});
+    else {
+        return res.status(400).json({ error: "User does not exist" });
     }
 }
 
@@ -38,17 +51,19 @@ const createAccount = async (req, res) => {
 
     if (!regEmail.test(account.username)) {
         console.log('400 Invalid Email');
-        return res.status(400).json({ success: false, error: 'Invalid email' })
+        return res.status(201).json({ success: false, message: 'Invalid email' })
     }
 
     else if (!account || account.pass === '') {
-        return res.status(400).json({ success: false, error: err })
+        return res.status(400).json({ success: false, message: 'Username and Password do not match' })
     }
 
     else {
         if (!userExists) {
-            const salt = await bcrypt.genSalt(10);
-            account.pass = await bcrypt.hash(account.pass.toString(), salt);
+            account.pass = await bcrypt.hash(account.pass.toString(), salt, (err, res)=>{
+                hash = res;
+                compare(hash);
+            });
             account.save().then(() => {
                 console.log(`Account created  ${account.pass}`);
                 return (res.status(201).json({
@@ -66,7 +81,7 @@ const createAccount = async (req, res) => {
         }
         else {
             console.log('401 User Already Exists');
-            return (res.status(401).json({ success: false, error: 'Username already exists' }))
+            return (res.status(400).json({ success: false, error: 'Username already exists' }))
         }
     }
 
